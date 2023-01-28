52cff38b8c59081ded9d89b956952e2e2044e1dd
S3B4 <hereniu.amaral.sebastian@gmail.com>
2023-01-27 22:55:24 -0300

    Ejercicio 2

---
+++ Ejercicio2/Algo.js
@@ -0,0 +1,25 @@
+// Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+
+// - Tu nombre (string)
+
+// - Tu edad (number)
+
+// - ¿Eres desarrollador? (boolean)
+
+// - Tu fecha de nacimiento (Date)
+
+// - Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const Datos= {	
+    nombre: "Juan", 
+    edad: 25,
+    esDesarrollador: true, 
+    fechaNacimiento: new Date(1995, 11, 17), 
+    libroFavorito: 
+        {
+            titulo: "El principito", 
+            autor: "Antoine de Saint-Exupéry", 
+            fecha: new Date(1943, 4, 6), 
+            url: "https://es.wikipedia.org/wiki/El_principito"
+        }
+            }
